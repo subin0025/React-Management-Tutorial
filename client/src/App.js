484e861e13a1d4647d23c8886e580120b7ecfc42
subin0025 <subin0025@naver.com>
2019-08-30 15:58:55 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -17,13 +18,18 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress :{
+    margin: theme.spacing.unit * 2
   }
 });
 class App extends Component {
   state = {
-    customers : ""
+    customers : "",
+    completed : 0
   }
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers:res}))
     .catch(err => console.log(err));
@@ -32,6 +38,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = ()=>{
+    const {completed} = this.state;
+    this.setState({completed : completed >= 100 ? 0 : completed +1});
   }
   render() {
   const { classes} = this.props;
@@ -49,7 +60,13 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} />) })
-            : ""}
+            :
+            <TableRow>
+              <TableCell colSpan='6' align='center'>
+                <CircularProgress className={classes.progress} variant='determinate' value={this.state.completed}/>
+              </TableCell>
+            </TableRow> 
+            }
           </TableBody>
         </Table>
       </Paper>
