52a67dfae3693c67ae85ff1e395f4eb69d62dc2c
subin0025 <subin0025@naver.com>
2019-09-01 17:03:30 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.Port || 5000;
-
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,24 +10,44 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: conf.host,
-    user : conf.user,
-    password : conf.password,
-    port : conf.port,
-    database : conf.database
+const multer = require('multer');
+const upload = multer({dest: './upload'})
 
+const connection = mysql.createConnection({
+host: conf.host,
+user: conf.user,
+password: conf.password,
+port: conf.port,
+database: conf.database
 });
 connection.connect();
 
 app.get('/api/customers', (req, res) => {
-   connection.query(
-       "SELECT * FROM CUSTOMER",
-       (err,rows,filds)=>{
-           res.send(rows);
-       }
-   )
-})
+connection.query(
+'SELECT * FROM CUSTOMER',
+(err, rows, fields) => {
+res.send(rows);
+}
+)
+});
 
-app.listen(port, () =>
-    console.log(`Listening on port ${port}`));
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+let image = '/image/' + req.file.filename;
+let name = req.body.name;
+let birthday = req.body.birthday;
+let gender = req.body.gender;
+let job = req.body.job;
+console.log(name);
+let params = [image, name, birthday, gender, job];
+connection.query(sql, params,
+(err, rows, fields) => {
+res.send(rows);
+}
+)
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
